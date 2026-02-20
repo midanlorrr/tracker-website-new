e03f4fdf650972a521d958b6922a278565724d42
midanlorrr <mikkodlorico@gmail.com>
2026-02-20 13:52:20 -0800

    Working current time countdown

--- index.js
+++ index.js
@@ -3,24 +3,61 @@ const Hours = document.getElementById('hours');
 const Minutes = document.getElementById('minutes');
 const Seconds = document.getElementById('seconds');
 
-let targetDateString = "February 21 2026 00:00:00";
+document.querySelector('[data-action="time-input"]').value = "2026-02-21T00:00"; // Default Date
+const timeInput = document.querySelector('[data-action="time-input"]'); // Grabs whatever the inputed time is
+let targetDate = new Date(timeInput.value).getTime(); // Turns that inputed time into the actual ms since 1970
 
-const targetDate = new Date(targetDateString).getTime();
+document.addEventListener('change', ()=>{
+    console.log('Date Changed');
+    targetDate = new Date(timeInput.value).getTime();
+})
 
-console.log(targetDate);
-console.log(Date.now());
+timer();
 
 function timer() {
     const diffMs = targetDate - Date.now();
-    console.log(diffMs);
     const days = Math.floor(diffMs / 1000 / 60 / 60 / 24);
-    console.log(days);
     const hours = Math.floor(diffMs / 1000 / 60 / 60) % 24;
-    console.log(hours);
     const minutes = Math.floor(diffMs / 1000 / 60) % 60;
-    console.log(minutes);
     const seconds = Math.floor(diffMs / 1000) % 60;
-    console.log(seconds);
+
+    //console.log(`${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds`);
+
+    Days.innerHTML = days;
+    Hours.innerHTML = hours;
+    Minutes.innerHTML = minutes;
+    Seconds.innerHTML = seconds;
 }
 
-timer();
+const intervalId = setInterval(timer, 1000);
+console.log(intervalId);
+
+document.addEventListener('click', (e)=>{
+    if (e.target.matches('[data-action="delete"]')) {
+        console.log('clicked');
+    }
+})
+/*
+timerGridHTML = `
+<div class="timer-example">
+    <h2>Task 1</h2>
+    <div class="timebox">
+    <div class="time-segments">
+        <h3 id="days">00</h3><p>Days</p>
+    </div>
+    <div class="time-segments">
+        <h3 id="hours">00</h3><p>Hours</p>
+    </div>
+    <div class="time-segments">
+        <h3 id="minutes">00</h3><p>Minutes</p>
+    </div>
+    <div class="time-segments">
+        <h3 id="seconds">00</h3><p>Seconds</p>
+    </div>
+    <input data-action="time-input" type="datetime-local" value="2026-02-21T00:00">
+    <button class="delete-button" data-action="delete">Delete</button>
+    </div>
+</div>
+`;*/
+
+//document.getElementById('js-timer-grid').innerHTML = timerGridHTML;
